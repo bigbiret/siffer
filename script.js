fbbf9e077c679033869d872ed2cf441d081ed016
bigbiret <105460409+bigbiret@users.noreply.github.com>
2022-05-18 22:09:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function calculate_digits() {
+ 
+  let num = document.getElementById('cardNumber').value;
+
+
+  document.getElementById('digitresult').innerHTML = num.length + '  siffer '
+  console.log(num)
+
+  console.log(num.length)
+}
